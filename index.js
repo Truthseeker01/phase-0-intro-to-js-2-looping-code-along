094d6a41a2e1cb96eded64de46a8f037cbd0fb85
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-02-23 16:31:19 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const newArray =[];
+function writeCards(names,eventName) {
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+function countDown(){
+    let i = 10;
+    while (i > -1){
+        console.log(i)
+        i--
+    };
+}
